bbe95f08d63d829b8890d1d741e8f9dabb4eae7f
Timka2501 <timurageev250113@gmail.com>
2025-11-20 20:05:21 +0300

    created main loop

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hello world!')
+from pygame import *
+
+window = display.set_mode((900, 800))
+display.set_caption('Pind-pong')
+
+game = True
+timer = time.Clock()
+
+while game:
+    window.fill((200, 220, 255))
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+        display.update()
+        timer.tick(60)
