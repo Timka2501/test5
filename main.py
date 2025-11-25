055e159ac0c94a8000ff49718270dd1d4fc86439
Timka2501 <timurageev250113@gmail.com>
2025-11-25 19:35:04 +0300

    added GameSprite class

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ window = display.set_mode((900, 800))
 display.set_caption('Pind-pong')
 
 game = True
-timer = time.Clock()
+timer
+
+class GameSprite(sprite.Sprite)
+    def __init__(self, filename, x, y, width=30, height=200)
+        super().__init__()
+        self.image = transform.scale(image.load(filename), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def draw(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 while game:
     window.fill((200, 220, 255))
